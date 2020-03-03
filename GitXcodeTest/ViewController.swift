b436988a383f0b2b212c5c40702cb96c3ff36951
a <1@1.com>
2020-03-03 17:48:13 +0300

    Add meat

---
+++ GitXcodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitXcodeTest
+//
+//  Created by Егор Красильников on 03/03/2020.
+//  Copyright © 2020 Егор Красильников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
